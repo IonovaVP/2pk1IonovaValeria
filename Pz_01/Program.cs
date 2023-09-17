6b58e70c75682f95f2f94fc87adb1ebfc217fd48
IonovaVP <hope.0908@yandex.ru>
2023-09-17 18:04:43 +0300

    добавлены 1 и 3 практики

---
+++ Pz_01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Pz_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            double b = Math.PI / 2;
+            double c = 2;
+            double result;
+
+            result = Math.Sqrt((a * b * c) / 2.4) - (0.7 * a * b * c) / Math.Sin(b) + Math.Pow(10, 4) * Math.Pow(Math.Abs(Math.Cos(b)), 1/5) - Math.Abs(b - a)/7.5;
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Pz_03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace Pz_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double day = int.Parse(Console.ReadLine());
+            switch (day)
+            {
+                case (<=10):
+                Console.WriteLine("первая декада месяца");
+                    break;
+
+                case (<= 20):
+                    Console.WriteLine("вторая декада месяца");
+                    break;
+
+                case (<= 31):
+                    Console.WriteLine("третья декада месяца");
+                        break;
+
+                default:
+                    Console.WriteLine("неправильная дата");
+                    break;
+
+            }
+        }
+    }
+}
